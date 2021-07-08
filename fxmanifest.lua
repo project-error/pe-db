5b8a7a5db1768320bae8b1db23d71713980e6bc3
Taso <taso@projecterror.dev>
2021-07-08 16:46:15 -0400

    Initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,6 @@
+fx_version 'cerulean'
+
+author 'Project Error'
+version '1.0.0'
+
+server_script 'dist/**/*'
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const { ProgressPlugin } = require('webpack')
+
+module.exports = {
+  entry: './src/index.ts',
+  output: {
+    filename: "[contenthash].js",
+    path: path.join(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: ['ts-loader'],
+        exclude: /node_modules/
+      }
+    ]
+  },
+  optimization: {
+    minimize: true
+  },
+  resolve: {
+    extensions: ['.ts']
+  },
+  plugins: [
+    new CleanWebpackPlugin({
+
+    }),
+    new ProgressPlugin()
+  ],
+  target: 'node'
+}
